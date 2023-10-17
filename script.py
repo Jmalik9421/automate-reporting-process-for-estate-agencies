54d51d64cb54b94d28e2895c31bdb74252f4feab
Jawad Malik <jmalik94@outlook.com>
2023-10-17 20:48:01 +0100

    installed Pillow in venv, corrected name error in import section in resetting_workbook_dimensions.py

---
+++ script.py
@@ -0,0 +1,11 @@
+# ------------------------ IMPORTING MODULES ------------------------ #
+import os
+import shutil
+import openpyxl
+import pyinputplus as pyip
+from PIL import Image as Image_PIL
+from openpyxl.drawing.image import Image as Image_openpyxl
+# ------------------------ IMPORTING MODULES ------------------------ #
+
+
+
