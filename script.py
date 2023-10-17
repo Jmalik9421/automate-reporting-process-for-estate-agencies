c83bc1806daaee61b4e79b634b07a0a7cec0a928
Jawad Malik <jmalik94@outlook.com>
2023-10-17 21:34:53 +0100

    grid coordinates primed, code reused from resetting_workbook_dimensions.py

--- script.py
+++ script.py
@@ -121,6 +121,30 @@ for index,name in enumerate(copies_images_list):
 
 
 
+# ------------------- INSERTING IMAGES TO WORKBOOK ------------------- #
+# ------- PRIMING GRID COORDINATES -------- # 
+# -- COLUMNS -- #
+columns = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+columns1 = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+columns2 = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+for letter1 in columns1:
+    for letter2 in columns2:
+        columns.append(letter1+letter2)                                 # 702 columns
+# -- COLUMNS -- #
+
+# --- ROWS ---- #
+rows = []                                                               # 702 rows
+for i in range(len(columns)):
+    i += 1                                                              # 1 index rows as in workbook
+    rows.append(i)
+# --- ROWS ---- #
+# ------- PRIMING GRID COORDINATES -------- # 
+
+# ------------------- INSERTING IMAGES TO WORKBOOK ------------------- #
+
+
+
 
 
 
